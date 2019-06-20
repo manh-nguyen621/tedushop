aff02c847983f72ea5cb1027a4ed29bb06c8db72
Manh Nguyen <manhnguyen12061997@gmail.com>
2019-06-20 09:21:02 +0700

    bai 6

---
+++ TeduShop.Data/Infrastructure/Diposeable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public class Diposeable : IDisposable
+    {
+        private bool isDisposed;
+        ~Diposeable()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if(!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+            isDisposed = true;
+        }
+
+        protected virtual void DisposeCore()
+        {
+
+        }
+    }
+}
---
+++ TeduShop.Data/Infrastructure/RepositoryBase.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public abstract class RepositoryBase<T> where T : class
+    {
+        private TeduShopDbContext dataContext;
+        private readonly IDbSet<T> dbSet;
+
+        protected IDbFactory DbFactory
+        {
+            get;
+            private set;
+        }
+
+        protected TeduShopDbContext DbContext
+        {
+            get { return dataContext ?? (dataContext = DbFactory.Init()); }
+        }
+
+        protected RepositoryBase(IDbFactory dbFactory)
+        {
+            DbFactory = dbFactory;
+            dbSet = DbContext.Set<T>();
+        }
+
+        public virtual void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            dbSet.Attach(entity);
+            dataContext.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public virtual void DeleteMulti(Expression<Func<T,bool>> where)
+        {
+            IEnumerable<T> objects = dbSet.Where<T>(where).AsEnumerable();
+            foreach (T obj in objects)
+                dbSet.Remove(obj);
+        }
+
+        public virtual T GetSingleById(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual IEnumerable<T> GetMany(Expression<Func<T,bool>> where, string includes)
+        {
+            return dbSet.Where(where).ToList();
+        }
+
+        public virtual int Count(Expression<Func<T,bool>> where)
+        {
+            return dbSet.Count(where);
+        }
+
+        public IQueryable<T> GetAll(string[] includes = null)
+        {
+            if(includes != null && includes.Count() > 0)
+            {
+                var query = dataContext.Set<T>().Include(includes.First());
+                foreach (var include in includes.Skip(1))
+                    query = query.Include(include);
+                return query.AsQueryable();
+            }
+            return dataContext.Set<T>().AsQueryable();
+        }
+
+        public T GetSingleByCondition(Expression<Func<T,bool>> expression, string[] includes = null)
+        {
+            return GetAll(includes).FirstOrDefault(expression);
+        }
+
+        public virtual IQueryable<T> GetMulti(Expression<Func<T,bool>> predicate, string[] includes = null)
+        {
+            if(includes != null && includes.Count() > 0)
+            {
+                var query = dataContext.Set<T>().Include(includes.First());
+                foreach (var include in includes.Skip(1))
+                    query = query.Include(include);
+                return query.Where<T>(predicate).AsQueryable<T>();
+            }
+            return dataContext.Set<T>().Where<T>(predicate).AsQueryable<T>();
+        }
+
+        //public virtual IQueryable<T> GetMultiPaging(Expression<Func<T,bool>> predicate, out int total, int index = 0, int size = 0)
+
+    }
+}
